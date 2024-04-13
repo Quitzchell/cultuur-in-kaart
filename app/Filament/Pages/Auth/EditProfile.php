ca6d963ce7c96ef73641386bc1eca187b15a65bf
mitchellquitz <mitchellquitz@gmail.com>
2024-04-13 16:36:31 +0200

    add custom EditProfile

---
+++ app/Filament/Pages/Auth/EditProfile.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Filament\Pages\Auth;
+
+use Filament\Forms\Components\Component;
+use Filament\Forms\Form;
+use Filament\Pages\Auth\EditProfile as BaseEditProfile;
+
+class EditProfile extends BaseEditProfile
+{
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                $this->getNameFormComponent(),
+                $this->getEmailFormComponent(),
+                $this->getPasswordFormComponent(),
+                $this->getPasswordConfirmationFormComponent()
+            ]);
+    }
+
+    protected function getPasswordFormComponent(): Component
+    {
+        return parent::getPasswordFormComponent()
+            ->validationMessages([
+                'same' => 'De invoer voor het nieuwe wachtwoorden komt niet overeen.'
+            ]);
+    }
+
+    protected function getPasswordConfirmationFormComponent(): Component
+    {
+        return parent::getPasswordConfirmationFormComponent()
+            ->visible();
+    }
+}
