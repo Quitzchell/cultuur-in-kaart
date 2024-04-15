b7080ada744f674877912caabcefc1cbfc21f451
mitchellquitz <mitchellquitz@gmail.com>
2024-04-15 12:49:52 +0200

    create Neighbourhood model

---
+++ app/Models/Neighbourhood.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Neighbourhood extends Model
+{
+    use HasFactory;
+
+    /* Relations */
+    public function activities(): HasMany
+    {
+        return $this->hasMany(Activity::class);
+    }
+}
