360919a8f97ccf44b68f86b70271f15e385b6731
mitchellquitz <mitchellquitz@gmail.com>
2024-04-20 14:48:32 +0200

    create ActivityRelationManager

---
+++ app/Filament/Resources/ActivityResource/RelationManagers/ActivityRelationManager.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Filament\Resources\ActivityResource\RelationManagers;
+
+use App\Models\Activity;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\RelationManagers\RelationManager;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class ActivityRelationManager extends RelationManager
+{
+    protected static string $relationship = 'relatedActivities';
+
+
+    public function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('name')
+                    ->required()
+                    ->maxLength(255),
+            ]);
+    }
+
+    public function table(Table $table): Table
+    {
+        return $table
+            ->query(function (Builder $query) {
+                return Activity::query()
+                    ->where('project_id', $this->ownerRecord->project_id)
+                    ->WhereNot('id', $this->ownerRecord->getKey());
+            })
+            ->recordTitleAttribute('activities')
+            ->columns([
+                Tables\Columns\TextColumn::make('name'),
+            ])
+            ->filters([
+                //
+            ])
+            ->headerActions([
+                Tables\Actions\CreateAction::make(),
+            ])
+            ->actions([
+                Tables\Actions\ViewAction::make(),
+                Tables\Actions\EditAction::make(),
+            ]);
+    }
+}
