fa01cf93cb817cae4412389a557a930776474430
mitchellquitz <mitchellquitz@gmail.com>
2024-04-21 20:19:29 +0200

    prepare database for primary coordinator and coordinator relations on Project

--- database/migrations/2024_04_12_213958_create_projects_table.php
+++ database/migrations/2024_04_12_213958_create_projects_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Coordinator;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -15,6 +16,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->unsignedInteger('project_number');
+            $table->foreignIdFor(Coordinator::class, 'primary_coordinator_id')->nullable();
             $table->date('start_date')->nullable();
             $table->date('end_date')->nullable();
             $table->float('budget_spend')->nullable();
