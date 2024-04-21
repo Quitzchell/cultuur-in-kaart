fb7a78e476b13103afd33aa1e5667c66acd74b91
mitchellquitz <mitchellquitz@gmail.com>
2024-04-21 20:59:30 +0200

    implement MoneyCast

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Casts\MoneyCast;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
@@ -12,6 +13,12 @@ use Illuminate\Database\Eloquent\Relations\HasOne;
 class Project extends Model
 {
     use HasFactory;
+
+    /* Casts */
+
+    protected $casts = [
+        'budget_spend' => MoneyCast::class,
+    ];
 
     /* Relations */
 
