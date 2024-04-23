36aecc43fe29860cc01ea638ce312b75552ab155
mitchellquitz <mitchellquitz@gmail.com>
2024-04-23 17:54:02 +0200

    add PartnerContactPerson entry for infolist

---
+++ app/Infolists/Components/PartnerContactPersonEntry.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Infolists\Components;
+
+use Filament\Infolists\Components\Entry;
+
+class PartnerContactPersonEntry extends Entry
+{
+    protected string $view = 'infolists.components.partner-contact-person-entry';
+}
