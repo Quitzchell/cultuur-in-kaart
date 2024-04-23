6fd966566bcc8b0223f5840195f4928caca014b6
mitchellquitz <mitchellquitz@gmail.com>
2024-04-23 17:42:53 +0200

    add contactPerson relationship to Partner

--- app/Models/Partner.php
+++ app/Models/Partner.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Partner extends Model
@@ -19,5 +20,10 @@ class Partner extends Model
     public function contactPeople(): BelongsToMany
     {
         return $this->belongsToMany(ContactPerson::class);
+    }
+
+    public function contactPerson(): BelongsTo
+    {
+        return $this->belongsTo(ContactPerson::class);
     }
 }
