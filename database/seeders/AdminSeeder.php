40581465e4b82f3b5f8280cc1e797afcddd38757
mitchellquitz <mitchellquitz@gmail.com>
2024-04-29 23:04:09 +0200

    refactor AdminSeeder.php

--- database/seeders/AdminSeeder.php
+++ database/seeders/AdminSeeder.php
@@ -14,7 +14,7 @@ class AdminSeeder extends Seeder
     public function run(): void
     {
         if (app()->environment() !== 'production') {
-            $admin = [
+            Coordinator::create([
                 'name' => 'Mitchell Quitz',
                 'role' => Role::Administrator->value,
                 'email' => 'mitchell@soc.nl',
@@ -22,19 +22,17 @@ class AdminSeeder extends Seeder
                 'phone' => '0628301804',
                 'password' => Hash::make('password'),
                 'remember_token' => Str::random(10),
-            ];
-        } else {
-            $admin = [
-                'name' => 'Melanie Leeflang',
-                'role' => Role::Administrator->value,
-                'email' => 'melanie@soc.nl',
-                'email_verified_at' => now(),
-                'phone' => '0638045527',
-                'password' => Hash::make('password'),
-                'remember_token' => Str::random(10),
-            ];
+            ]);
         }
 
-        Coordinator::create($admin);
+        Coordinator::create([
+            'name' => 'Melanie Leeflang',
+            'role' => Role::Administrator->value,
+            'email' => 'melanie@soc.nl',
+            'email_verified_at' => now(),
+            'phone' => '0638045527',
+            'password' => Hash::make('password'),
+            'remember_token' => Str::random(10),
+        ]);
     }
 }
