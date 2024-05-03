daa87ef4a6c812a0eafd031d2704ac43ba647b24
mitchellquitz <mitchellquitz@gmail.com>
2024-05-03 13:16:42 +0200

    create setup for testing

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -2,9 +2,28 @@
 
 namespace Tests;
 
+use App\Models\Coordinator;
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
 abstract class TestCase extends BaseTestCase
 {
-    //
+    protected function setUp(): void
+    {
+        parent::setUp();
+
+        config(['database.default' => 'sqlite']);
+        config(['database.connections.sqlite.database' => ':memory:']);
+        $this->artisan('migrate');
+
+        $coordinator = Coordinator::factory()->create();
+
+        $this->actingAs($coordinator);
+    }
+
+    protected function tearDown(): void
+    {
+        $this->artisan('migrate:rollback');
+
+        parent::tearDown();
+    }
 }
