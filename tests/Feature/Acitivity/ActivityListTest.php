8f6b924205cf0e73afcdbfd7a34e846d483b3d07
mitchellquitz <mitchellquitz@gmail.com>
2024-05-04 14:02:12 +0200

    create tests for ActivityList

--- tests/Feature/Acitivity/ActivityListTest.php
+++ tests/Feature/Acitivity/ActivityListTest.php
@@ -2,17 +2,18 @@
 
 use App\Filament\Resources\ActivityResource;
 use App\Models\Activity;
+use App\Models\Neighbourhood;
 use App\Models\Project;
+use App\Models\Task;
 use function Pest\Livewire\livewire;
 
+/** Rendering */
 it('can render Activity List', function () {
     $this->get(ActivityResource::getUrl())->assertSuccessful();
 });
 
 it('can list activities', function () {
-    $activities = Activity::factory()
-        ->count(10)
-        ->create();
+    $activities = Activity::factory()->count(10)->create();
 
     livewire(ActivityResource\Pages\ListActivities::class)
         ->assertCanSeeTableRecords($activities)
@@ -27,10 +28,9 @@ it('can list activities', function () {
         ->assertTableColumnExists('neighbourhoods.name');
 });
 
+/** Sorting */
 it('can sort activities by date', function () {
-    $activities = Activity::factory()
-        ->count(10)
-        ->create();
+    $activities = Activity::factory()->count(5)->create();
 
     livewire(ActivityResource\Pages\ListActivities::class)
         ->sortTable('date')
@@ -39,11 +39,9 @@ it('can sort activities by date', function () {
         ->assertCanSeeTableRecords($activities->sortByDesc('date'), inOrder: true);
 });
 
+/** searching */
 it('can search activities by name', function () {
-    $activities = Activity::factory()
-        ->count(10)
-        ->create();
-
+    $activities = Activity::factory()->count(10)->create();
     $name = $activities->first()->name;
 
     livewire(ActivityResource\Pages\ListActivities::class)
@@ -53,12 +51,114 @@ it('can search activities by name', function () {
 });
 
 it('can search activities by project name', function () {
-    $projects = Project::factory()->count(4)->has(Activity::factory()->count(10))->create();
-
+    $projects = Project::factory()->count(4)->has(Activity::factory()->count(5))->create();
     $project = $projects->first();
 
     livewire(ActivityResource\Pages\ListActivities::class)
         ->searchTable($project->name)
         ->assertCanSeeTableRecords($project->activities->where('project_id', $project->getKey()))
         ->assertCanNotSeeTableRecords($project->activities->where('project_id', '!==', $project->getKey()));
+});
+
+it('can search activities by neighbourhood name', function () {
+    $neighbourhoods = Neighbourhood::factory()->count(10)->create();
+    $activities = Activity::factory(5)->create()->each(function (Activity $activity) use ($neighbourhoods) {
+        $activity->neighbourhoods()->attach($neighbourhoods->random(2));
+    });
+
+    $neighbourhood = $neighbourhoods->first();
+    $filteredActivities = $activities->filter(function (Activity $activity) use ($neighbourhood) {
+        return $activity->neighbourhoods->contains($neighbourhood);
+    });
+
+    livewire(ActivityResource\Pages\ListActivities::class)
+        ->searchTable($neighbourhood->name)
+        ->assertCanSeeTableRecords($filteredActivities)
+        ->assertCanNotSeeTableRecords($activities->diff($filteredActivities));
+});
+
+/** Filtering */
+it('can filter activities by project', function () {
+    $projects = Project::factory()->count(4)->create();
+    $activities = Activity::factory()->count(10)->create()->each(function (Activity $activity) use ($projects) {
+        $activity->project()->associate($projects->random());
+        $activity->save();
+    });
+    $project = $projects->first();
+
+    livewire(ActivityResource\Pages\ListActivities::class)
+        ->assertCanSeeTableRecords($activities)
+        ->filterTable('project_id', $project->getKey())
+        ->assertCanSeeTableRecords($activities->where('project_id', $project->getKey()))
+        ->assertCanNotSeeTableRecords($activities->where('project_id', '!==', $project->getKey()));
+});
+
+it('can filter activities by neighbourhood', function () {
+    $neighbourhoods = Neighbourhood::factory()->count(10)->create();
+    $activities = Activity::factory()->count(10)->create()->each(function (Activity $activity) use ($neighbourhoods) {
+        $activity->neighbourhoods()->attach($neighbourhoods->random(2));
+    });
+
+    $neighbourhood = $activities->first()->neighbourhoods->first();
+    $filteredActivities = $activities->filter(function (Activity $activity) use ($neighbourhood) {
+        return $activity->neighbourhoods->contains($neighbourhood);
+    });
+
+    livewire(ActivityResource\Pages\ListActivities::class)
+        ->assertCanSeeTableRecords($activities)
+        ->filterTable('neighbourhood_id', $neighbourhood->getKey())
+        ->assertCanSeeTableRecords($filteredActivities)
+        ->assertCanNotSeeTableRecords($activities->diff($filteredActivities));
+});
+
+it('can filter activities by task', function () {
+    $tasks = Task::factory()->count(10)->create();
+    $activities = Activity::factory()->count(10)->create()->each(function (Activity $activity) use ($tasks) {
+        $activity->task()->associate($tasks->random());
+        $activity->save();
+    });
+    $task = $tasks->first();
+
+    livewire(ActivityResource\Pages\ListActivities::class)
+        ->assertCanSeeTableRecords($activities)
+        ->filterTable('task_id', $task->getKey())
+        ->assertCanSeeTableRecords($activities->where('task_id', $task->getKey()))
+        ->assertCanNotSeeTableRecords($activities->where('task_id', '!==', $task->getKey()));
+});
+
+it('can filter activities by date from', function () {
+    $activities = Activity::factory()->count(10)->create();
+    $sortedActivities = $activities->sortBy('date');
+    $dateFrom = $sortedActivities->get(5)->date;
+
+    livewire(ActivityResource\Pages\ListActivities::class)
+        ->assertCanSeeTableRecords($activities)
+        ->filterTable('date', ['date_from' => $dateFrom, 'date_until' => null])
+        ->assertCanSeeTableRecords($activities->where('date', '>=', $dateFrom))
+        ->assertCanNotSeeTableRecords($activities->where('date', '<', $dateFrom));
+});
+
+it('can filter activities by date until', function () {
+    $activities = Activity::factory()->count(10)->create();
+    $sortedActivities = $activities->sortBy('date');
+    $dateUntil = $sortedActivities->get(5)->date;
+
+    livewire(ActivityResource\Pages\ListActivities::class)
+        ->assertCanSeeTableRecords($activities)
+        ->filterTable('date', ['date_from' => null, 'date_until' => $dateUntil])
+        ->assertCanSeeTableRecords($activities->where('date', '<=', $dateUntil))
+        ->assertCanNotSeeTableRecords($activities->where('date', '>', $dateUntil));
+});
+
+it('can filter activities by date between', function () {
+    $activities = Activity::factory()->count(10)->create();
+    $sortedActivities = $activities->sortBy('date');
+    $dateFrom = $sortedActivities->get(3)->date;
+    $dateUntil = $sortedActivities->get(9)->date;
+
+    livewire(ActivityResource\Pages\ListActivities::class)
+        ->assertCanSeeTableRecords($activities)
+        ->filterTable('date', ['date_from' => $dateFrom, 'date_until' => $dateUntil])
+        ->assertCanSeeTableRecords($activities->where('date', '>=', $dateFrom)->where('date', '<=', $dateUntil))
+        ->assertCanNotSeeTableRecords($activities->where('date', '<', $dateFrom)->where('date', '>', $dateUntil));
 });
