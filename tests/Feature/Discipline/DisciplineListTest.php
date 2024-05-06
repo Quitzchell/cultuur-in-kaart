a79c4661f35d2f529182cf0463cf7965dc7e5bf2
mitchellquitz <mitchellquitz@gmail.com>
2024-05-06 22:25:56 +0200

    create DisciplineListTest

---
+++ tests/Feature/Discipline/DisciplineListTest.php
@@ -0,0 +1,21 @@
+<?php
+
+use App\Filament\Resources\DisciplineResource;
+use App\Filament\Resources\DisciplineResource\Pages\ListDisciplines;
+use App\Models\Discipline;
+use function Pest\Livewire\livewire;
+
+/** Rendering */
+it('can render Discipline list', function () {
+    $this->get(DisciplineResource::getUrl())->assertSuccessful();
+});
+
+it('can list Disciplines', function () {
+    $discipline = Discipline::factory(10)->create();
+
+    livewire(ListDisciplines::class)
+        ->assertCanSeeTableRecords($discipline)
+        ->assertCountTableRecords(10)
+        ->assertCanRenderTableColumn('name')
+        ->assertTableColumnExists('name');
+});
