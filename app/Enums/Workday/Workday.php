18e7772b36618e8f25082edf26c73e0bb484154c
mitchellquitz <mitchellquitz@gmail.com>
2024-05-06 21:35:17 +0200

    refactor Workday enum

--- app/Enums/Workday/Workday.php
+++ app/Enums/Workday/Workday.php
@@ -4,13 +4,13 @@ namespace App\Enums\Workday;
 
 enum Workday: string
 {
-    case Maandag = 'Maandag';
-    case Dinsdag = 'Dinsdag';
-    case Woensdag = 'Woensdag';
-    case Donderdag = 'Donderdag';
-    case Vrijdag = 'Vrijdag';
-    case Zaterdag = 'Zaterdag';
-    case Zondag = 'Zondag';
+    case Monday = 'Maandag';
+    case Tuesday = 'Dinsdag';
+    case Wednesday = 'Woensdag';
+    case Thursday = 'Donderdag';
+    case Friday = 'Vrijdag';
+    case Saturday = 'Zaterdag';
+    case Sunday = 'Zondag';
 
     public static function labels(): array
     {
@@ -22,12 +22,22 @@ enum Workday: string
         return $labels;
     }
 
+    public static function values(): array
+    {
+        $values = [];
+        foreach (self::cases() as $case) {
+            $values[] = $case->value;
+        }
+
+        return $values;
+    }
+
     public static function toArray(): array
     {
         $casesToArray = [];
         foreach (self::cases() as $case) {
             $casesToArray[$case->name] = $case->value;
-        };
+        }
 
         return $casesToArray;
     }
