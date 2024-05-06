34fd1c0f680e41dee9a286852300012a52338d65
mitchellquitz <mitchellquitz@gmail.com>
2024-05-07 00:11:05 +0200

    create NeighbourhoodListTest

---
+++ tests/Feature/Neighbourhood/NeighbourhoodListTest.php
@@ -0,0 +1,19 @@
+<?php
+
+use App\Filament\Resources\NeighbourhoodResource;
+use App\Models\Neighbourhood;
+use function Pest\Livewire\livewire;
+
+it('can render Neighbourhood list', function () {
+    $this->get(NeighbourhoodResource::getUrl())->assertSuccessful();
+});
+
+it('can List Neighbourhoods', function () {
+    $neighbourhood = Neighbourhood::factory(10)->create();
+
+    livewire(NeighbourhoodResource\Pages\ListNeighbourhoods::class)
+        ->assertCanSeeTableRecords($neighbourhood)
+        ->assertCountTableRecords(10)
+        ->assertCanRenderTableColumn('name')
+        ->assertTableColumnExists('name');
+});
