d2d096e823a51a870015a30e8305ecb4486df710
mitchellquitz <mitchellquitz@gmail.com>
2024-05-06 21:58:40 +0200

    extend DisciplineFormTest

--- tests/Feature/Discipline/DisciplineFormTest.php
+++ tests/Feature/Discipline/DisciplineFormTest.php
@@ -6,11 +6,17 @@ use App\Models\Discipline;
 use function Pest\Livewire\livewire;
 
 /** Rendering */
-it('can render discipline form', function () {
+it('can render discipline create form', function () {
     $discipline = Discipline::factory()->create();
     $this->get(DisciplineResource::getUrl('create', ['record' => $discipline->getKey()]))->assertSuccessful();
 });
 
+it('can render discipline edit form', function () {
+    $discipline = Discipline::factory()->create();
+    $this->get(DisciplineResource::getUrl('edit', ['record' => $discipline->getKey()]))->assertSuccessful();
+});
+
+/** Creation */
 it('can create discipline', function () {
     $discipline = Discipline::factory()->make();
 
@@ -23,4 +29,14 @@ it('can create discipline', function () {
     $this->assertDatabaseHas(Discipline::class, [
         'name' => $discipline->name,
     ]);
+});
+
+it('can validate discipline form', function () {
+    livewire(CreateDiscipline::class)
+        ->fillForm([
+            'name' => null
+        ])->call('create')
+        ->assertHasFormErrors([
+            'name' => 'required'
+        ]);
 });
