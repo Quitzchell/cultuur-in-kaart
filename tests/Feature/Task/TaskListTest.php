b23113ceb8f8b58b2dc99aef9990dc53cfc07827
mitchellquitz <mitchellquitz@gmail.com>
2024-05-07 00:20:43 +0200

    fix naming on TaskListTest

--- tests/Feature/Task/TaskListTest.php
+++ tests/Feature/Task/TaskListTest.php
@@ -9,11 +9,11 @@ it('can render Task list', function () {
     $this->get(TaskResource::getUrl())->assertSuccessful();
 });
 
-it('can List Neighbourhoods', function () {
-    $neighbourhood = Task::factory(10)->create();
+it('can List Tasks', function () {
+    $task = Task::factory(10)->create();
 
     livewire(ListTasks::class)
-        ->assertCanSeeTableRecords($neighbourhood)
+        ->assertCanSeeTableRecords($task)
         ->assertCountTableRecords(10)
         ->assertCanRenderTableColumn('name')
         ->assertTableColumnExists('name');
