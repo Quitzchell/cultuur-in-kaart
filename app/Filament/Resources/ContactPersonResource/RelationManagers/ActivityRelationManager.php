0e6f906b39e750f3a01c48666a2dc461cce2b124
mitchellquitz <mitchellquitz@gmail.com>
2024-05-10 19:05:27 +0200

    clean code

--- app/Filament/Resources/ContactPersonResource/RelationManagers/ActivityRelationManager.php
+++ app/Filament/Resources/ContactPersonResource/RelationManagers/ActivityRelationManager.php
@@ -5,6 +5,8 @@ namespace App\Filament\Resources\ContactPersonResource\RelationManagers;
 use App\Filament\Resources\ActivityResource;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
+use Filament\Tables\Actions\ViewAction;
+use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
 
 
@@ -17,18 +19,18 @@ class ActivityRelationManager extends RelationManager
         return $table
             ->recordTitleAttribute('name')
             ->columns([
-                Tables\Columns\TextColumn::make('date')
+                TextColumn::make('date')
                     ->label('Datum')
                     ->date('d-m-Y'),
-                Tables\Columns\TextColumn::make('name')
+                TextColumn::make('name')
                     ->label('Activiteitnaam'),
-                Tables\Columns\TextColumn::make('task.name')
+                TextColumn::make('task.name')
                     ->label('Taak'),
-                Tables\Columns\TextColumn::make('neighbourhoods.name')
+                TextColumn::make('neighbourhoods.name')
                     ->label('Wijken')
                     ->default('-')
                     ->limit(40),
-                Tables\Columns\TextColumn::make('partners.name')
+                TextColumn::make('partners.name')
                     ->label('Samenwerkingspartners')
                     ->default('-')
                     ->limit(40),
@@ -36,11 +38,8 @@ class ActivityRelationManager extends RelationManager
             ->filters([
                 //
             ])
-            ->headerActions([
-                //
-            ])
             ->actions([
-                Tables\Actions\ViewAction::make()
+                ViewAction::make()
                     ->label('')
                     ->url(fn($record): string => ActivityResource::getUrl('view', ['record' => $record])),
             ]);
