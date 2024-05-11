6aed7da923452174990cc6ef10a95f84a4f4306b
mitchellquitz <mitchellquitz@gmail.com>
2024-05-11 17:52:27 +0200

    Make ActivityRelationManager for Partner filterable on tasks

--- app/Filament/Resources/PartnerResource/RelationManagers/ActivityRelationManager.php
+++ app/Filament/Resources/PartnerResource/RelationManagers/ActivityRelationManager.php
@@ -5,6 +5,7 @@ namespace App\Filament\Resources\PartnerResource\RelationManagers;
 use App\Filament\Resources\ActivityResource;
 use Filament\Resources\RelationManagers\RelationManager;
 use Filament\Tables;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 
 class ActivityRelationManager extends RelationManager
@@ -27,31 +28,14 @@ class ActivityRelationManager extends RelationManager
                     ->searchable(),
                 Tables\Columns\TextColumn::make('task.name')
                     ->label('Taak'),
-                Tables\Columns\TextColumn::make('neighbourhoods.name')
-                    ->label('Wijken')
-                    ->formatStateUsing(function ($state) {
-                        $neighbourhoods = explode(', ', $state);
-                        sort($neighbourhoods);
-                        return implode(', ', $neighbourhoods);
-                    })
-                    ->placeholder('-')
-                    ->limit(40),
-                Tables\Columns\TextColumn::make('partners.name')
-                    ->label('Andere samenwerkingspartners')
-                    ->limit(40)
-                    ->formatStateUsing(function (string $state, ?Activity $record) {
-                        $filteredArray = array_filter(explode(', ', $state), function ($partnerName) use ($record) {
-                            return $partnerName !== Partner::find($record->partner_id)->name;
-                        });
-                        return !empty($filteredArray) ? implode(', ', $filteredArray) : '-';
-                    }),
             ])
             ->filters([
-                //
-            ])
-            ->headerActions([
-                //
-            ])
+                SelectFilter::make('task')
+                    ->relationship('task', 'name')
+                    ->label('Taken')
+                    ->preload()
+                    ->multiple(),
+            ], layout: Tables\Enums\FiltersLayout::Modal)
             ->actions([
                 Tables\Actions\ViewAction::make()
                     ->label('')
