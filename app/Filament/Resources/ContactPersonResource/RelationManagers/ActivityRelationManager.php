8376b1791c9cff6cdd46e83e4b990e7ea6c67980
mitchellquitz <mitchellquitz@gmail.com>
2024-05-12 01:17:36 +0200

    add task and filter for ActivityRelationManager for ContactPerson

--- app/Filament/Resources/ContactPersonResource/RelationManagers/ActivityRelationManager.php
+++ app/Filament/Resources/ContactPersonResource/RelationManagers/ActivityRelationManager.php
@@ -3,10 +3,13 @@
 namespace App\Filament\Resources\ContactPersonResource\RelationManagers;
 
 use App\Filament\Resources\ActivityResource;
+use Filament\Forms\Components\Section;
 use Filament\Resources\RelationManagers\RelationManager;
-use Filament\Tables;
+use Filament\Support\Enums\MaxWidth;
 use Filament\Tables\Actions\ViewAction;
 use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Enums\FiltersLayout;
+use Filament\Tables\Filters\SelectFilter;
 use Filament\Tables\Table;
 
 
@@ -28,7 +31,24 @@ class ActivityRelationManager extends RelationManager
                 TextColumn::make('name')
                     ->label('Activiteitnaam')
                     ->searchable(),
+                TextColumn::make('task.name')
+                    ->label('Taak'),
             ])
+            ->filters([
+                SelectFilter::make('task')
+                    ->relationship('task', 'name')
+                    ->columnSpan(1)
+                    ->label('Taken')
+                    ->preload()
+                    ->multiple(),
+            ], FiltersLayout::Modal)
+            ->filtersFormSchema(fn(array $filters): array => [
+                Section::make()
+                    ->schema([
+                        $filters['task']
+                    ]),
+            ])
+            ->filtersFormWidth(MaxWidth::ExtraLarge)
             ->actions([
                 ViewAction::make()
                     ->label('')
