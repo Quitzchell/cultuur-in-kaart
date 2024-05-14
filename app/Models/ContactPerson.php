bd1ddd9a89f911c823ecb795815b1aeb06b43bd9
mitchellquitz <mitchellquitz@gmail.com>
2024-05-14 21:02:30 +0200

    "Update the relationship to accommodate the new pivot table for contact person, partner and activities, which includes additional values to incorporate the partner associated with the contact person through the activity."

--- app/Models/ContactPerson.php
+++ app/Models/ContactPerson.php
@@ -5,7 +5,6 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class ContactPerson extends Model
@@ -13,9 +12,14 @@ class ContactPerson extends Model
     use HasFactory;
 
     /* Relations */
-    public function activities(): HasMany
+    public function activities(): HasManyThrough
     {
-        return $this->hasMany(Activity::class);
+        return $this->hasManyThrough(
+            Activity::class,
+            ActivityContactPersonPartner::class,
+            secondKey: 'id',
+            secondLocalKey: 'activity_id'
+        );
     }
 
     public function projects(): HasManyThrough
