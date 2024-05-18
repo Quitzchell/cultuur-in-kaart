899d83bbe0e8d53cb4e620182d3918c5bc470bf4
mitchellquitz <mitchellquitz@gmail.com>
2024-05-18 21:22:29 +0200

    create Discipline model

---
+++ app/Models/Discipline.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Discipline extends Model
+{
+    use HasFactory;
+
+    /* Relations */
+    public function activities(): HasMany
+    {
+        return $this->hasMany(Activity::class);
+    }
+}
