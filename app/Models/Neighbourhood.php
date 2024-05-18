70877ebd93fa8e0e60832bf7b0ffa513c7ca90e5
mitchellquitz <mitchellquitz@gmail.com>
2024-05-18 17:49:30 +0200

    define coordinators relation on Neighbourhood.php

--- app/Models/Neighbourhood.php
+++ app/Models/Neighbourhood.php
@@ -4,8 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Neighbourhood extends Model
 {
     use HasFactory;
+
+    /* Relations */
+    public function coordinators(): BelongsToMany
+    {
+        return $this->belongsToMany(Coordinator::class);
+    }
 }
