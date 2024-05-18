89cc38d2ddcc54a01e1f636507f73e20b8d96349
mitchellquitz <mitchellquitz@gmail.com>
2024-05-18 17:50:53 +0200

    create ProjectFactory.php

---
+++ database/factories/ProjectFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Project;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends Factory<Project>
+ */
+class ProjectFactory extends Factory
+{
+    public function definition(): array
+    {
+        $startDate = $this->faker->dateTimeThisMonth()->format('Y-m-d');
+        $endDate = $this->faker->dateTimeBetween($startDate, '+1 month')->format('Y-m-d');
+
+        return [
+            'name' => $this->faker->name(),
+            'project_number' => $this->faker->randomNumber(),
+            'start_date' => $startDate,
+            'end_date' => $endDate,
+            'budget_spend' => $this->faker->numberBetween(0, 1000000),
+        ];
+    }
+}
