11263f90346d37e7a145f9710cf48b1165e71c79
mitchellquitz <mitchellquitz@gmail.com>
2024-05-18 17:51:02 +0200

    update DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,9 @@
 
 namespace Database\Seeders;
 
+use App\Models\Coordinator;
+use App\Models\Neighbourhood;
+use App\Models\Project;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,6 +17,21 @@ class DatabaseSeeder extends Seeder
         $this->call([
             AdminSeeder::class,
             NeighbourhoodSeeder::class
-           ]);
+        ]);
+
+        $this->createModels();
+
+
+        foreach (Coordinator::all() as $coordinator) {
+            $coordinator->neighbourhoods()->attach(Neighbourhood::all()->random(2));
+            $coordinator->projects()->attach(Project::all()->random(2));
+        }
+
+    }
+
+    private function createModels(): void
+    {
+        Coordinator::factory(2)->create();
+        Project::factory(5)->create();
     }
 }
