993a58b218182e46fae869441f6778ebd587fdf5
mitchellquitz <mitchellquitz@gmail.com>
2024-05-18 20:33:21 +0200

    create Activity model and factory

---
+++ app/Models/Activity.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Activity extends Model
+{
+    use HasFactory;
+
+    /* Relations */
+    public function task(): BelongsTo
+    {
+        return $this->belongsTo(Task::class);
+    }
+
+    public function project(): BelongsTo
+    {
+        return $this->belongsTo(Project::class);
+    }
+
+    public function neighbourhood(): BelongsTo
+    {
+        return $this->belongsTo(Neighbourhood::class);
+    }
+
+    public function coordinators(): BelongsToMany
+    {
+        return $this->belongsToMany(Coordinator::class);
+    }
+}
---
+++ database/migrations/2024_05_18_190356_create_activities_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Models\Neighbourhood;
+use App\Models\Project;
+use App\Models\Task;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->date('date');
+            $table->text('comment')->nullable();
+            $table->foreignIdFor(Task::class)->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Project::class)->nullable()->constrained()->cascadeOnDelete();
+            $table->foreignIdFor(Neighbourhood::class)->nullable()->constrained()->cascadeOnDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activities');
+    }
+};
