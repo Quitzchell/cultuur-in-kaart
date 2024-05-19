b8f05add99468b782e3affe56b186e4045c3e63e
mitchellquitz <mitchellquitz@gmail.com>
2024-05-19 11:44:26 +0200

    add relations to ContactPerson.php

--- app/Models/ContactPerson.php
+++ app/Models/ContactPerson.php
@@ -4,8 +4,26 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class ContactPerson extends Model
 {
     use HasFactory;
+
+    /* Relations */
+    public function activities(): belongsToMany
+    {
+        return $this->belongsToMany(Partner::class)->with('activity');
+    }
+
+    public function partners(): BelongsToMany
+    {
+        return $this->belongsToMany(Partner::class);
+    }
+
+
+    public function projects(): belongsToMany
+    {
+        return $this->belongsToMany(Partner::class)->with('activity')->with('project');
+    }
 }
