8f84ea011ab537ebc6d37070905e3c42b5e23d43
mitchellquitz <mitchellquitz@gmail.com>
2024-05-19 21:34:25 +0200

    refactor ContactPerson relations for RelationManagers

--- app/Models/ContactPerson.php
+++ app/Models/ContactPerson.php
@@ -2,18 +2,25 @@
 
 namespace App\Models;
 
+use App\Models\Pivots\ActivityPartnerContactPerson;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasManyThrough;
 
 class ContactPerson extends Model
 {
     use HasFactory;
 
     /* Relations */
-    public function activities(): belongsToMany
+    public function activities(): HasManyThrough
     {
-        return $this->belongsToMany(Partner::class)->with('activity');
+        return $this->hasManyThrough(
+            Activity::class,
+            ActivityPartnerContactPerson::class,
+            secondKey: 'id',
+            secondLocalKey: 'activity_id',
+        );
     }
 
     public function partners(): BelongsToMany
@@ -21,9 +28,13 @@ class ContactPerson extends Model
         return $this->belongsToMany(Partner::class);
     }
 
-
-    public function projects(): belongsToMany
+    public function projects(): HasManyThrough
     {
-        return $this->belongsToMany(Partner::class)->with('activity')->with('project');
+        return $this->hasManyThrough(
+            Activity::class,
+            ActivityPartnerContactPerson::class,
+            secondKey: 'id',
+            secondLocalKey: 'activity_id',
+        )->leftJoin('projects', 'projects.id', 'activities.project_id');
     }
 }
