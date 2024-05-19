764e32aaabcb150f553917c738fba95018a0d26c
mitchellquitz <mitchellquitz@gmail.com>
2024-05-19 15:44:56 +0200

    clean code

--- app/Models/Pivots/ContactPersonPartner.php
+++ app/Models/Pivots/ContactPersonPartner.php
@@ -4,8 +4,6 @@ namespace App\Models\Pivots;
 
 use App\Models\ContactPerson;
 use App\Models\Partner;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\Pivot;
 
