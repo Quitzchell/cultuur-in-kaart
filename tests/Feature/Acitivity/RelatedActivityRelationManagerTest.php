ffe05e56231530ba52f9070b236c6a78e319a9ef
mitchellquitz <mitchellquitz@gmail.com>
2024-05-20 14:10:47 +0200

    refactor tests

--- tests/Feature/Acitivity/RelatedActivityRelationManagerTest.php
+++ tests/Feature/Acitivity/RelatedActivityRelationManagerTest.php
@@ -1,74 +1,74 @@
 <?php
-//
-//use App\Filament\Resources\ActivityResource\Pages\ViewActivities;
-//use App\Filament\Resources\ActivityResource\RelationManagers\RelatedActivityRelationManager;
-//use App\Models\Activity;
-//use App\Models\Project;
-//use App\Models\Task;
-//use function Pest\Livewire\livewire;
-//
-///** Render */
-//it('can render related Activities', function () {
-//    $activity = Activity::factory()->create();
-//    livewire(RelatedActivityRelationManager::class, [
-//        'ownerRecord' => $activity,
-//        'pageClass' => ViewActivities::class
-//    ])->assertSuccessful();
-//});
-//
-//it('can list related Activities', function () {
-//    $tasks = Task::factory(10)->create();
-//    $activities = Activity::factory(10)->create(['project_id' => Project::factory()->create()->getKey()])
-//        ->each(function (Activity $activity) use ($tasks) {
-//            $activity->task()->associate($tasks->random());
-//            $activity->save();
-//        });
-//
-//    livewire(RelatedActivityRelationManager::class, [
-//        'ownerRecord' => $activities->first(),
-//        'pageClass' => ViewActivities::class
-//    ])->assertCanSeeTableRecords($activities)
-//        ->assertCountTableRecords(9)
-//        ->assertCanRenderTableColumn('date')
-//        ->assertTableColumnExists('date')
-//        ->assertCanRenderTableColumn('name')
-//        ->assertTableColumnExists('name')
-//        ->assertCanRenderTableColumn('task.name')
-//        ->assertTableColumnExists('task.name');
-//});
-//
-///** Sort */
-//it('can sort related Activities by date', function () {
-//    $activities = Activity::factory(10)
-//        ->create(['project_id' => Project::factory()->create()->getKey()]);
-//
-//    $activity = $activities->first();
-//    $activities = $activities->where('id', '!==', $activity->getKey());
-//    livewire(RelatedActivityRelationManager::class, [
-//        'ownerRecord' => $activity,
-//        'pageClass' => ViewActivities::class
-//    ])->sortTable('date')
-//        ->assertCanSeeTableRecords($activities->sortBy('date'), inOrder: true)
-//        ->sortTable('date', 'desc')
-//        ->assertCanSeeTableRecords($activities->sortByDesc('date'), inOrder: true);
-//});
-//
-///** Filter */
-//it('can filter RelatedActivities on Task', function () {
-//    $tasks = Task::factory(10)->create();
-//    $activities = Activity::factory(10)->create(['project_id' => Project::factory()->create()->getKey()])
-//        ->each(function (Activity $activity) use ($tasks) {
-//            $activity->task()->associate($tasks->random());
-//            $activity->save();
-//        });
-//
-//    $activity = $activities->first();
-//    $activities = $activities->where('id', '!==', $activity->getKey());
-//    livewire(RelatedActivityRelationManager::class, [
-//        'ownerRecord' => $activity,
-//        'pageClass' => ViewActivities::class
-//    ])->assertCanSeeTableRecords($activities)
-//        ->filterTable('task', $activity->task_id)
-//        ->assertCanSeeTableRecords($activities->where('task_id', $activity->task_id))
-//        ->assertCanNotSeeTableRecords($activities->where('task_id', '!==', $activity->task_id));
-//});
+
+use App\Filament\RelationManagers\ActivityResource\ActivityRelationManager;
+use App\Filament\Resources\ActivityResource\Pages\ViewActivity;
+use App\Models\Activity;
+use App\Models\Project;
+use App\Models\Task;
+use function Pest\Livewire\livewire;
+
+/** Render */
+it('can render related Activities', function () {
+    $activity = Activity::factory()->create();
+    livewire(ActivityRelationManager::class, [
+        'ownerRecord' => $activity,
+        'pageClass' => ViewActivity::class
+    ])->assertSuccessful();
+});
+
+it('can list related Activities', function () {
+    $tasks = Task::factory(10)->create();
+    $activities = Activity::factory(10)->create(['project_id' => Project::factory()->create()->getKey()])
+        ->each(function (Activity $activity) use ($tasks) {
+            $activity->task()->associate($tasks->random());
+            $activity->save();
+        });
+
+    livewire(ActivityRelationManager::class, [
+        'ownerRecord' => $activities->first(),
+        'pageClass' => ViewActivity::class
+    ])->assertCanSeeTableRecords($activities)
+        ->assertCountTableRecords(9)
+        ->assertCanRenderTableColumn('date')
+        ->assertTableColumnExists('date')
+        ->assertCanRenderTableColumn('name')
+        ->assertTableColumnExists('name')
+        ->assertCanRenderTableColumn('task.name')
+        ->assertTableColumnExists('task.name');
+});
+
+/** Sort */
+it('can sort related Activities by date', function () {
+    $activities = Activity::factory(10)
+        ->create(['project_id' => Project::factory()->create()->getKey()]);
+
+    $activity = $activities->first();
+    $activities = $activities->where('id', '!==', $activity->getKey());
+    livewire(ActivityRelationManager::class, [
+        'ownerRecord' => $activity,
+        'pageClass' => ViewActivity::class
+    ])->sortTable('date')
+        ->assertCanSeeTableRecords($activities->sortBy('date'), inOrder: true)
+        ->sortTable('date', 'desc')
+        ->assertCanSeeTableRecords($activities->sortByDesc('date'), inOrder: true);
+});
+
+/** Filter */
+it('can filter related Activities on Task', function () {
+    $tasks = Task::factory(10)->create();
+    $activities = Activity::factory(10)->create(['project_id' => Project::factory()->create()->getKey()])
+        ->each(function (Activity $activity) use ($tasks) {
+            $activity->task()->associate($tasks->random());
+            $activity->save();
+        });
+
+    $activity = $activities->first();
+    $activities = $activities->where('id', '!==', $activity->getKey());
+    livewire(ActivityRelationManager::class, [
+        'ownerRecord' => $activity,
+        'pageClass' => ViewActivity::class
+    ])->assertCanSeeTableRecords($activities)
+        ->filterTable('task', $activity->task_id)
+        ->assertCanSeeTableRecords($activities->where('task_id', $activity->task_id))
+        ->assertCanNotSeeTableRecords($activities->where('task_id', '!==', $activity->task_id));
+});
--- tests/Feature/Project/PartnerRelationManagerTest.php
+++ tests/Feature/Project/PartnerRelationManagerTest.php
@@ -1,92 +1,103 @@
 <?php
-//
-//use App\Filament\Resources\ProjectResource\Pages\ViewProjects;
-//use App\Filament\Resources\ProjectResource\RelationManagers\PartnerRelationManager;
-//use App\Models\Activity;
-//use App\Models\Partner;
-//use App\Models\Project;
-//use function Pest\Livewire\livewire;
-//
-//it('can render related Partners', function () {
-//    $project = Project::factory()->create();
-//    livewire(PartnerRelationManager::class, [
-//        'ownerRecord' => $project,
-//        'pageClass' => ViewProjects::class
-//    ])->assertSuccessful();
-//});
-//
-//it('can list related Partners', function () {
-//    $project = Project::factory()->create();
-//    $activities = Activity::factory(10)->create()->each(function (Activity $activity) use ($project) {
-//        $activity->project()->associate($project);
-//        $activity->save();
-//    });
-//
-//    $partners = Partner::factory()->count(3)->create()->each(function (Partner $partner) use ($activities) {
-//        $partner->activities()->attach($activities);
-//    });
-//
-//    livewire(PartnerRelationManager::class, [
-//        'ownerRecord' => $project,
-//        'pageClass' => ViewProjects::class
-//    ])->assertCanSeeTableRecords($partners);
-//});
-//
-//it('can search related Partners by name', function () {
-//    $project = Project::factory()->create();
-//    $activity = Activity::factory()->create();
-//    $activity->project()->associate($project);
-//    $activity->save();
-//    $partners = Partner::factory()->count(10)->create()->each(function (Partner $partner) use ($activity) {
-//        $partner->activities()->attach($activity);
-//    });
-//
-//    $name = $partners->first()->name;
-//    livewire(PartnerRelationManager::class, [
-//        'ownerRecord' => $project,
-//        'pageClass' => ViewProjects::class
-//    ])->searchTable($name)
-//        ->assertCanSeeTableRecords($partners->where('name', $name))
-//        ->assertCanNotSeeTableRecords($partners->where('name', '!==', $name));
-//});
-//
-//it('can search related Partners by contact person', function () {
-//    $project = Project::factory()->create();
-//    $activity = Activity::factory()->create();
-//    $activity->project()->associate($project);
-//    $activity->save();
-//    $partners = Partner::factory()->count(10)->create()->each(function (Partner $partner) use ($activity) {
-//        $partner->activities()->attach($activity);
-//    });
-//
-//    $contactPerson = $partners->first()->primaryContactPerson;
-//    $filteredPartners = $partners->filter(function (Partner $partner) use ($contactPerson) {
-//        return $partner->primaryContactPerson === $contactPerson;
-//    });
-//
-//    livewire(PartnerRelationManager::class, [
-//        'ownerRecord' => $project,
-//        'pageClass' => ViewProjects::class
-//    ])->searchTable($contactPerson)
-//        ->assertCanSeeTableRecords($filteredPartners)
-//        ->assertCanNotSeeTableRecords($partners->diff($filteredPartners));
-//});
-//
-//it('can filter related Partners by neighbourhood', function () {
-//    $project = Project::factory()->create();
-//    $activity = Activity::factory()->create();
-//    $activity->project()->associate($project);
-//    $activity->save();
-//    $partners = Partner::factory()->count(10)->create()->each(function (Partner $partner) use ($activity) {
-//        $partner->activities()->attach($activity);
-//    });
-//
-//    $partner = $partners->first();
-//    livewire(PartnerRelationManager::class, [
-//        'ownerRecord' => $project,
-//        'pageClass' => ViewProjects::class
-//    ])->assertCanSeeTableRecords($partners)
-//        ->filterTable('neighbourhood', $partner->neighbourhood)
-//        ->assertCanSeeTableRecords($partners->where('neighbourhood', $partner->neighbourhood))
-//        ->assertCanNotSeeTableRecords($partners->where('neighbourhood', '!==', $partner->neighbourhood));
-//});
+
+
+use App\Filament\RelationManagers\ProjectResource\PartnerRelationManager;
+use App\Filament\Resources\ProjectResource\Pages\ViewProject;
+use App\Models\Activity;
+use App\Models\ContactPerson;
+use App\Models\Partner;
+use App\Models\Project;
+use Illuminate\Database\Eloquent\Factories\Sequence;
+use function Pest\Livewire\livewire;
+
+it('can render related Partners', function () {
+    $project = Project::factory()->create();
+    livewire(PartnerRelationManager::class, [
+        'ownerRecord' => $project,
+        'pageClass' => ViewProject::class
+    ])->assertSuccessful();
+});
+
+it('can list related Partners', function () {
+    $project = Project::factory()->create();
+    $activities = Activity::factory(10)->create(['project_id' => $project->id]);
+    $partners = Partner::factory(2)->create();
+    $contactPerson = ContactPerson::factory()->create();
+    foreach ($partners as $partner) {
+        $partner->contactPerson()->associate($contactPerson->getKey());
+        $partner->save();
+    }
+
+    foreach ($activities as $activity) {
+        $activity->partners()->attach($partners->random());
+    }
+
+    livewire(PartnerRelationManager::class, [
+        'ownerRecord' => $project,
+        'pageClass' => ViewProject::class
+    ])->assertCanSeeTableRecords($partners);
+});
+
+it('can search related Partners by name', function () {
+    $project = Project::factory()->create();
+    $activities = Activity::factory(100)->create(['project_id' => $project->id]);
+    $partners = Partner::factory(2)->state(new Sequence(['name' => 'Fabriek Magnifiek'], ['name' => 'Visserij Rijstenbrij']))->create();
+    foreach ($activities as $key => $activity) {
+        $activity->partners()->attach($partners[$key % 2]);
+    }
+
+    $name = $partners->first()->name;
+    livewire(PartnerRelationManager::class, [
+        'ownerRecord' => $project,
+        'pageClass' => ViewProject::class
+    ])->searchTable($name)
+        ->assertCanSeeTableRecords($partners->where('name', $name))
+        ->assertCanNotSeeTableRecords($partners->where('name', '!==', $name));
+});
+
+it('can search related Partners by contact person', function () {
+    $project = Project::factory()->create();
+    $activities = Activity::factory(100)->create(['project_id' => $project->id]);
+    $partners = Partner::factory(2)->state(new Sequence(['name' => 'Fabriek Magnifiek'], ['name' => 'Visserij Rijstenbrij']))->create();
+    $contactPersons = ContactPerson::factory(2)->create();
+    foreach ($partners as $key => $partner) {
+        $partner->contactPerson()->associate($contactPersons[$key % 2]->getKey());
+        $partner->save();
+    }
+
+    foreach ($activities as $key => $activity) {
+        $activity->partners()->attach($partners[$key % 2]);
+    }
+
+    $contactPerson = $partners->first()->contactPerson;
+    $filteredPartners = $partners->filter(function (Partner $partner) use ($contactPerson) {
+        return $partner->contactPerson->name === $contactPerson->name;
+    });
+
+    livewire(PartnerRelationManager::class, [
+        'ownerRecord' => $project,
+        'pageClass' => ViewProject::class
+    ])->searchTable($contactPerson->name)
+        ->assertCanSeeTableRecords($filteredPartners)
+        ->assertCanNotSeeTableRecords($partners->diff($filteredPartners));
+});
+
+it('can filter related Partners by neighbourhood', function () {
+    $project = Project::factory()->create();
+    $activity = Activity::factory()->create();
+    $activity->project()->associate($project);
+    $activity->save();
+    $partners = Partner::factory()->count(10)->create()->each(function (Partner $partner) use ($activity) {
+        $partner->activities()->attach($activity);
+        $partner->save();
+    });
+
+    $partner = $partners->first();
+    livewire(PartnerRelationManager::class, [
+        'ownerRecord' => $project,
+        'pageClass' => ViewProject::class
+    ])->assertCanSeeTableRecords($partners)
+        ->filterTable('neighbourhood', $partner->neighbourhood)
+        ->assertCanSeeTableRecords($partners->where('neighbourhood', $partner->neighbourhood))
+        ->assertCanNotSeeTableRecords($partners->where('neighbourhood', '!==', $partner->neighbourhood));
+});
