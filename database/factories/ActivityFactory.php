220f1b09c822356e443afc06655e4e6a1a1dc7d4
mitchellquitz <mitchellquitz@gmail.com>
2024-05-28 19:33:05 +0200

    update ActivityFactory.php

--- database/factories/ActivityFactory.php
+++ database/factories/ActivityFactory.php
@@ -18,7 +18,7 @@ class ActivityFactory extends Factory
     public function definition(): array
     {
         return [
-            'name' => $this->faker->word(),
+            'name' => $this->faker->unique()->word(),
             'date' => $this->faker->date(),
             'comment' => $this->faker->text(),
         ];
